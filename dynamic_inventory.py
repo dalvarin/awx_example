b2725c79bf8b4255afd20458977ef7f29e64b192
dalvarin <47025234+dalvarin@users.noreply.github.com>
2025-04-21 11:51:56 +0200

    Create dynamic_inventory.py

---
+++ dynamic_inventory.py
@@ -0,0 +1,13 @@
+def main():
+    inventory = {
+        "all": {
+            "hosts": ["host1.example.com", "host2.example.com"],
+            "vars": {
+                "ansible_user": "your_user"
+            }
+        }
+    }
+    print(json.dumps(inventory))
+
+if __name__ == "__main__":
+    main()
