be1fed0ea87459863ef440719804c14da55e4fa8
dalvarin <47025234+dalvarin@users.noreply.github.com>
2025-04-21 13:23:13 +0200

    Update dynamic_inventory.py

--- dynamic_inventory.py
+++ dynamic_inventory.py
@@ -1,13 +1,15 @@
+import json
+
 def main():
-    inventory = {
-        "all": {
-            "hosts": ["host1.example.com", "host2.example.com"],
-            "vars": {
-                "ansible_user": "your_user"
-            }
-        }
-    }
-    print(json.dumps(inventory))
+  inventory = {
+    "all": {
+      "hosts": ["host1.example.com", "host2.example.com"],
+      "vars": {
+        "ansible_user": "your_user"
+      }
+    }
+  }
+  print(json.dumps(inventory))
 
 if __name__ == "__main__":
-    main()
+  main()
